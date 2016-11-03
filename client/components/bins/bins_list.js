29bcc7854c468b13f627418f4cb20e54ab44e46e
Eduardo Henrique de Oliveira <eduardocurva@gmail.com>
2016-11-03 12:10:58 +0000

    markdown view and sharing functionality

--- client/components/bins/bins_list.js
+++ client/components/bins/bins_list.js
@@ -43,5 +43,7 @@ class BinsList extends Component{
 export default createContainer(()=>{
     Meteor.subscribe('bins');
 
+    Meteor.subscribe('sharedBins');
+    
     return {bins: Bins.find({}).fetch()};
 }, BinsList);
--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -16,7 +16,23 @@ Meteor.methods({
     'bins.remove': function(bin){
         return Bins.remove(bin);
         
+    },
+
+    'bins.update': function(bin, content){
+        return Bins.update(bin._id, { $set: { content }});
+
+    },
+
+    'bins.share': function(bin, email){
+        
+        //push = push a new record into the array
+        //The line below means:
+        //find the bin with this id; find the array property called
+        //sharedWith and push email as new value
+        return Bins.update(bin._id, {$push: {sharedWith: email }});
+
     }
+
 });
 
 export const Bins = new Mongo.Collection('bins');
